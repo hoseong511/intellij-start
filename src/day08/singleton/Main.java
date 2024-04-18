304619136c1bb68a574a71a5a6593fcf3e92fb98
hoseong511 <thdghtjd115@naver.com>
2024-04-18 12:29:49 +0900

    feat day08-singleton

--- src/day08/singleton/Main.java
+++ src/day08/singleton/Main.java
@@ -1,5 +1,7 @@
-package day08.singleton;/**
- *packageName    : day08.singleton
+package day08.singleton;
+
+/**
+ * packageName    : day08.singleton
  * fileName       : Main
  * author         : hoho
  * date           : 4/18/24
@@ -8,5 +10,17 @@ package day08.singleton;/**
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 4/18/24        hoho       최초 생성
- */public class Main {
+ */
+public class Main {
+    public static void main(String[] args) {
+        Mypet m1 = Mypet.getInstance();
+        Mypet m2 = Mypet.getInstance();
+        Mypet m3 = Mypet.getInstance();
+
+        m1.setName("hoho2");
+        System.out.println("m1 = " + m1.getName());
+        System.out.println("m2 = " + m2.getName());
+        System.out.println("m3 = " + m3.getName());
+
+    }
 }
--- src/day08/singleton/Mypet.java
+++ src/day08/singleton/Mypet.java
@@ -1,5 +1,7 @@
-package day08.singleton;/**
- *packageName    : day08.singleton
+package day08.singleton;
+
+/**
+ * packageName    : day08.singleton
  * fileName       : Mypet
  * author         : hoho
  * date           : 4/18/24
@@ -8,5 +10,40 @@ package day08.singleton;/**
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 4/18/24        hoho       최초 생성
- */public class Mypet {
+ * singletone pattern : 클래스에서 인스턴스 하나만 생성하도록 제한하는 방식
+ * 1. 생성자 private 처리
+ * 2. 스스로 인스턴스 생성
+ * 3. 공개된 메서드(static)를 통해 생성해둔 인스턴스 제공
+ */
+public class Mypet {
+    String name;
+    private static Mypet obj = new Mypet();
+
+    private Mypet() {
+
+    }
+
+    /**
+     * static으로 method 제공
+     *
+     * @return Mypet type
+     * @method getInstance
+     * @author hoho
+     * @date 2024 04 18 11:56
+     */
+    static public Mypet getInstance() {
+        return obj;
+    }
+
+    void introduce() {
+        System.out.printf("내 반려동물의 이름은 %s\n", this.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
