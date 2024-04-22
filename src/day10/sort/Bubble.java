d1cd3f504b99ceacb0f94b186854d1b0d97a4589
hoseong511 <thdghtjd115@naver.com>
2024-04-22 15:59:59 +0900

    feat day10-sort

--- src/day10/sort/Bubble.java
+++ src/day10/sort/Bubble.java
@@ -1,8 +1,27 @@
-package day10.sort;/**
- *packageName    : day10.sort
+package day10.sort;
+
+import java.util.Arrays;
+
+
+/**
+ * packageName    : day10.sort
  * fileName       : Bubble
  * author         : hoho
  * date           : 4/22/24
  * description    :
- */public class Bubble {
+ */
+public class Bubble {
+	public static void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
 }
--- src/day10/sort/BubbleTest.java
+++ src/day10/sort/BubbleTest.java
@@ -1,10 +1,22 @@
+package day10.sort;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
- *packageName    : day10.sort
+ * packageName    : day10.sort
  * fileName       : BubbleTest
  * author         : hoho
  * date           : 4/22/24
  * description    :
- */class BubbleTest {
-  
+ */
+class BubbleTest {
+	@Test
+	@DisplayName("bubble sort")
+	public void bubble() {
+		Bubble.sort(new int[]{3, 4, 1, -1, 6, 22});
+	}
+
 }
